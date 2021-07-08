0b8db6c8a5e5c80634990f9f52c8777000e6755e
dmweis <dmweis@users.noreply.github.com>
2021-07-08 02:52:30 +0000

    deploy: e3c6a0b8ee9efa6c02900e23818a15e34eb7a9d4

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["hopper_face"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["hopper_face"] = {"name":"","files":["animations.rs","lib.rs"]};
 sourcesIndex["simple"] = {"name":"","files":["simple.rs"]};
 createSourceSidebar();
